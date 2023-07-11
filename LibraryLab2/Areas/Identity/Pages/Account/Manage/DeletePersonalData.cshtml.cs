7946ca962d56b4161684fa0cc99784fedc218d6c
KatarzynaAmeliaGrzanka <katarzyna.a.grzanka@gmail.com>
2023-07-11 16:18:40 +0200

    Upload

---
+++ LibraryLab2/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
@@ -0,0 +1,143 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Threading.Tasks;
+using LibraryLab2.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NuGet.Versioning;
+
+namespace LibraryLab2.Areas.Identity.Pages.Account.Manage
+{
+    
+
+    public class DeletePersonalDataModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<DeletePersonalDataModel> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public DeletePersonalDataModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<DeletePersonalDataModel> logger,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _context = context;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public bool RequirePassword { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var books = from s in _context.Book.Where(j => j.User == user.UserName && j.Leased != null)
+                        select s;
+
+            if (books.Count() != 0)
+            {
+                TempData["AlertMessage"] = "It's not possible to delete your account because you have some books leased!";
+            }
+            else
+            {
+                RequirePassword = await _userManager.HasPasswordAsync(user);
+                return Page();
+            }
+            return Redirect("~/");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            var books = from s in _context.Book.Where(a => a.Leased != null && a.User == user.UserName)
+                        select s;
+            if (books.Count() != 0)
+            {
+                TempData["AlertMessage"] = "It is not possible to delete your account - you have some books leased.";
+                return Redirect("~/");
+            }
+
+
+            RequirePassword = await _userManager.HasPasswordAsync(user);
+            if (RequirePassword)
+            {
+                if (!await _userManager.CheckPasswordAsync(user, Input.Password))
+                {
+                    ModelState.AddModelError(string.Empty, "Incorrect password.");
+                    return Page();
+                }
+            }
+
+            var reservations = from s in _context.Book.Where(a => a.Reserved != null && a.User == user.UserName)
+                        select s;
+            foreach (var reservation in reservations)
+            {
+                    reservation.Reserved = null;
+                    reservation.User = null;
+            }
+
+            _context.SaveChanges();
+
+            var result = await _userManager.DeleteAsync(user);
+            var userId = await _userManager.GetUserIdAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Unexpected error occurred deleting user.");
+            }                  
+
+            await _signInManager.SignOutAsync();
+
+            _logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
+
+            return Redirect("~/");
+        }
+    }
+}
---
+++ LibraryLab2/Controllers/BooksController.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryLab2.Areas.Identity.Data;
+using LibraryLab2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+
+
+namespace LibraryLab2.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            expireReservations();
+            return View(await _context.Book.ToListAsync());
+        }
+
+        // Displayking search page
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            expireReservations();
+            return View();
+        }
+
+        // Displayking search results
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            expireReservations();
+            return View("Index", await _context.Book.Where(j => j.Title.Contains(SearchPhrase)).ToListAsync());
+        }
+
+        // When user is logged he can display his/her reservations
+        [Authorize]
+        public async Task<IActionResult> Reservations()
+        {
+
+            expireReservations();
+            return View(await _context.Book.Where(j => j.User == this.User.Identity.Name && j.Leased == null).ToListAsync());
+        }
+
+        // GET: Books/Details/5    (Not used)
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+    
+           
+        // User can reserve book
+        // - date of next day is stored because then the reservation will expire
+        // - 
+        public async Task<IActionResult> Reserve(int id)
+        {
+            expireReservations();
+            if (_context.Book == null)
+            {
+                return Problem("LibraryContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+                
+                    DateTime date = DateTime.Today;
+                    DateTime newDate = date.AddDays(1);
+                    book.Reserved = newDate.ToShortDateString();
+                    var name = User.Identity.Name;
+                    book.User = name;
+            }
+
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Librarian can rent a book that is reserved
+        public async Task<IActionResult> Rent(int id)
+        {
+            expireReservations();
+            if (_context.Book == null)
+            {
+                return Problem("'LibraryContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+
+                book.Leased = book.User;
+            }
+
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Librarian can mark when a book is returned
+        public async Task<IActionResult> Return(int id)
+        {
+            expireReservations();
+            if (_context.Book == null)
+            {
+                return Problem("'LibraryContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+
+                book.Leased = null;
+                book.User = null;
+                book.Reserved = null;
+            }
+
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // User can delete reservations he did
+        public async Task<IActionResult>DeleteReservation(int id)
+        {
+            if (_context.Book == null)
+            {
+                return Problem("Entity set 'LibraryContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+                book.Reserved = null;
+                book.User = null;
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Reservations));
+        }
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Publisher,Author")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Publisher,Author,Date,User,Reserved,Leased")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Book == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+                _context.Book.Remove(book);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+          return _context.Book.Any(e => e.Id == id);
+        }
+
+        // Function that updates reservations
+        private void expireReservations()
+        {
+            string todaysDate = DateTime.Today.ToShortDateString();
+            foreach (var book in _context.Book)
+            {
+                if (string.Compare(book.Reserved, todaysDate) < 0)
+                {
+                    book.Reserved = null;
+                    book.User = null;
+                }
+            }
+            return;
+        }
+    }
+}
